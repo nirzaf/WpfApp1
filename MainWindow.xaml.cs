703e92fc47359fd50df6649a753459d9ef5e9c91
M.F.M Fazrin <mfmfazrin1986@gmail.com>
2019-10-15 20:25:19 +0530

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Resources["bgGreen"] = new SolidColorBrush(Colors.White);
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Outer Button Clicked");
+        }
+
+        private void InnerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Inner Button Clicked");
+        }
+
+
+        private void InnerInnerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Top Button Clicked");
+        }
+    }
+}
